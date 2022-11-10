9e341f5fe5c3253657c15ecebe968816c8342f91
Patrick Legare <p_legare@hotmail.com>
2022-11-10 11:00:18 -0500

    model update for user

--- libs/users/src/lib/+state/users.models.ts
+++ libs/users/src/lib/+state/users.models.ts
@@ -4,4 +4,9 @@
 export interface UsersEntity {
     id: string | number; // Primary ID
     name: string;
+    hourlyRate: number;
+    weeklyHours: number;
+    dailyCommuteHours: number;
+    grossIncome: number;
+    netIncome: number
 }
