2f64a970a106dfa0cf957e6ceb6c9677a4b7eb4b
Patrick Legare <p_legare@hotmail.com>
2022-11-10 20:19:30 -0500

    tax data

---
+++ libs/tax/src/lib/data/tax_ca_qc.ts
@@ -0,0 +1,90 @@
+export const tax_ca_qc = {
+    "consummerTaxes": [
+        {
+            "name": "gst",
+            "rate": "0.15"
+        },
+        {
+            "name": "gst",
+            "rate": "0.4"
+        }
+    ],
+    "incomeTaxes": [
+        {
+            "name": "RRQ",
+            "rates": [
+                {
+                    "max": 3500,
+                    "rate": 0
+                },
+                {
+                    "max": 64900,
+                    "rate": 0.0615
+                }
+            ]
+        },
+        {
+            "name": "RQAP",
+            "rates": [
+                {
+                    "max": 88000,
+                    "rate": 0.00494
+                }
+            ]
+        },
+        {
+            "name": "ProvincialTaxes",
+            "rates": [
+                {
+                    "max": 16143,
+                    "rate": 0
+                },
+                {
+                    "max": 46295,
+                    "rate": 0.15
+                },
+                {
+                    "max": 92580,
+                    "rate": 0.20
+                },
+                {
+                    "max": 112655,
+                    "rate": 0.24
+                },
+                {
+                    "max": 9999999999,
+                    "rate": 0.2575
+                }
+            ]
+        },
+        {
+            "name": "FederalTaxes",
+            "rates": [
+                {
+                    "max": 14398,
+                    "rate": 0
+                },
+                {
+                    "max": 50197,
+                    "rate": 0.15
+                },
+                {
+                    "max": 100392,
+                    "rate": 0.205
+                },
+                {
+                    "max": 155625,
+                    "rate": 0.26
+                },
+                {
+                    "max": 221708,
+                    "rate": 0.29
+                },
+                {
+                    "max": 9999999999,
+                    "rate": 0.33
+                }
+            ]
+        }
+    ]
+}
