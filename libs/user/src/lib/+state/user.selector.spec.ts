3ec8ce1092ba00e537cb85ad4ceb867e1776e779
Patrick Legare <p_legare@hotmail.com>
2022-11-11 11:29:29 -0500

    user gross and net income selectors.

--- libs/user/src/lib/+state/user.selector.spec.ts
+++ libs/user/src/lib/+state/user.selector.spec.ts
@@ -4,6 +4,8 @@ import {
     selectDailyCommute,
     selectGrossIncome,
     selectNetIncome,
+    selectCalcGrossIncome,
+    selectCalcNetIncome,
 } from './user.selector';
 
 import { User } from './user.model';
@@ -36,21 +38,63 @@ describe('User selector', () => {
         expect(selectDailyCommute(state)).toEqual(2);
     });
 
-    test('selectGrossIncome', () => {
-        const state = {
-            user: <User>{
-                grossIncome: 50000,
-            },
-        };
-        expect(selectGrossIncome(state)).toEqual(50000);
+    describe('selectGrossIncome', () => {
+        test('with grossIncome', () => {
+            const state = {
+                user: <User>{
+                    grossIncome: 50000,
+                },
+            };
+            expect(selectGrossIncome(state)).toEqual(50000);
+        });
+    
+        test('with calcGrossIncome', () => {
+            const state = {
+                user: <User>{
+                    hourlyRate: 30,
+                    weeklyHours: 40,
+                },
+            };
+            expect(selectGrossIncome(state)).toEqual(62400);
+        });
+
+        test('with nothing', () => {
+            expect(selectGrossIncome({user: {}})).toEqual(0);
+        });
     });
 
-    test('selectNetIncome', () => {
-        const state = {
-            user: <User>{
-                netIncome: 30000,
-            },
-        };
-        expect(selectNetIncome(state)).toEqual(30000);
+    describe('selectNetIncome', () => {
+
+        test('with netIncome', () => {
+            const state = {
+                user: <User>{
+                    netIncome: 30000,
+                },
+            };
+            expect(selectNetIncome(state)).toEqual(30000);
+        });
+
+        test('with predefined grossIncome', () => {
+            const state = {
+                user: <User>{
+                    grossIncome: 62400
+                },
+            };
+            expect(selectCalcNetIncome(state)).toEqual(42854.13);
+        });
+    
+        test('without calculated grossIncome', () => {
+            const state = {
+                user: <User>{
+                    hourlyRate: 30,
+                    weeklyHours: 40,
+                },
+            };
+            expect(selectCalcNetIncome(state)).toEqual(42854.13);
+        });
+
+        test('with nothing', () => {
+            expect(selectCalcNetIncome({user: {}})).toEqual(0);
+        });
     });
 });
--- libs/user/src/lib/+state/user.selector.ts
+++ libs/user/src/lib/+state/user.selector.ts
@@ -1,5 +1,6 @@
 import { createSelector, createFeatureSelector } from '@ngrx/store';
 import { User } from './user.model';
+import { getIncomeTaxesTotal, getNetIncome, tax_ca_qc } from '@fiscaliste/tax';
 
 export const featureKey = 'user';
 
@@ -24,12 +25,38 @@ export const selectDailyCommute = createSelector(
     (state: User) => state.dailyCommuteHours
 );
 
+export const selectCalcGrossIncome = createSelector(
+    selectHourlyRate,
+    selectWeeklyHours,
+    (hourlyRate, weeklyHours) => {
+        if (hourlyRate && weeklyHours) {
+            return hourlyRate * weeklyHours * 52; // TODO: replace 52 by a selector (user that works part time)
+        } else {
+            return 0;
+        }
+    }
+);
+
 export const selectGrossIncome = createSelector(
     selectFeature,
-    (state: User) => state.grossIncome
+    selectCalcGrossIncome,
+    (state: User, calcGrossIncome) => state.grossIncome ? state.grossIncome : calcGrossIncome || 0
+);
+
+export const selectCalcNetIncome = createSelector(
+    selectGrossIncome,
+    (grossIncome) => {
+        if (grossIncome) {
+            return getNetIncome(grossIncome, tax_ca_qc.incomeTaxes); // replace tax_ca_qc by root context state value.
+        } else {
+            return 0;
+        }
+    }
 );
 
 export const selectNetIncome = createSelector(
     selectFeature,
-    (state: User) => state.netIncome
+    selectCalcNetIncome,
+    (state: User, calcNetIncome: number) => state.netIncome ? state.netIncome : calcNetIncome || 0
 );
+
